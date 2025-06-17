283e4c626369caf82e9b7b20120ad539fe0eea66
Craveu <138560628+Yuxiax@users.noreply.github.com>
2025-06-17 16:03:33 +0800

    Update test1.py

--- test1.py
+++ test1.py
@@ -5,4 +5,5 @@ writer = tensorboard.SummaryWriter("./test")
 for i in range(1000):
     # tf.summary.scalar(f'2*i', float(2*i), i)
     # tf.summary.scalar(f'5*i', float(5 * i), i)
+    tf.summary.scalar(f'5*i', float(5 * i), i)
     writer.add_scalar(f'5*i', float(5 * i), i)
